874bb433a4fb3a318681cb90c3f8cfb9e0c43b9a
zhamilt98 <46009636+zhamilt98@users.noreply.github.com>
2024-01-25 17:39:42 -0500

    Add files via upload

---
+++ src/main/java/com/example/paratransit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paratransit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ src/main/java/com/example/paratransit/login.java
@@ -0,0 +1,4 @@
+package com.example.paratransit;
+
+public class login {
+}
